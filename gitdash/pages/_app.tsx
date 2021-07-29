90a5ac084f18c75456463de90b3e8acdb2e6355c
Anand Rajaram <anandrajaram21@gmail.com>
2021-07-29 11:51:35 +0530

    feat: GitHub OAuth now works

--- gitdash/pages/_app.tsx
+++ gitdash/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider session={pageProps.session}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
 export default MyApp;
