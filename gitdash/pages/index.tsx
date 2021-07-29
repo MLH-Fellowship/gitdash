ea51791f8268bcf2be0a617b3be080e37a4db80b
Anand Rajaram <anandrajaram21@gmail.com>
2021-07-29 17:32:48 +0530

    feat: completed the home page

--- gitdash/pages/index.tsx
+++ gitdash/pages/index.tsx
@@ -1,69 +1,24 @@
 import Head from "next/head";
-import {
-  Box,
-  Heading,
-  Container,
-  Text,
-  Button,
-  Stack,
-  Icon,
-  useColorModeValue,
-  createIcon,
-} from "@chakra-ui/react";
 
-export default function CallToActionWithAnnotation() {
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import Features from "../components/features";
+import Footer from "../components/footer";
+import Team from "../components/team";
+
+export default function HomePage() {
   return (
     <>
       <Head>
         <title>Git Dash</title>
       </Head>
-
-      <Container maxW={"3xl"}>
-        <Stack
-          as={Box}
-          textAlign={"center"}
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}
-        >
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-            lineHeight={"110%"}
-          >
-            Make money from <br />
-            <Text as={"span"} color={"green.400"}>
-              your audience
-            </Text>
-          </Heading>
-          <Text color={"gray.500"}>
-            Monetize your content by charging your most loyal readers and reward
-            them loyalty points. Give back to your loyal readers by granting
-            them access to your pre-releases and sneak-peaks.
-          </Text>
-          <Stack
-            direction={"column"}
-            spacing={3}
-            align={"center"}
-            alignSelf={"center"}
-            position={"relative"}
-          >
-            <Button
-              colorScheme={"green"}
-              bg={"green.400"}
-              rounded={"full"}
-              px={6}
-              _hover={{
-                bg: "green.500",
-              }}
-            >
-              Get Started
-            </Button>
-            <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
-              Learn more
-            </Button>
-          </Stack>
-        </Stack>
-      </Container>
+      <body>
+        <Navbar />
+        <Hero />
+        <Features />
+        <Team />
+        <Footer />
+      </body>
     </>
   );
 }
