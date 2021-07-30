fce2aac60b29bc43d9f605d2337705ad98f1961f
Green-Ranger11 <junior.eteuati@gmail.com>
2021-07-31 09:35:25 +1300

    Get Login To Work

--- gitdash/pages/api/github.ts
+++ gitdash/pages/api/github.ts
@@ -1,6 +1,6 @@
 const { Octokit } = require('@octokit/rest')
 
-export default async (req, res) => {
+export default async (_req: any, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { stars: any; followers: any; starred: any; all_repos: any; repo_count: any; issues: any; issue_count: any }): any; new(): any } } }) => {
   const octokit = new Octokit({
     // auth: 
     // github token for a particular user, leaving empty for now
@@ -15,7 +15,7 @@ export default async (req, res) => {
   const repos = await octokit.request(`/users/${username}/repos`);
 
   // Number of stars
-  const starsCount = repos.data.filter(repo => !repo.fork).reduce((acc, item) => {
+  const starsCount = repos.data.filter((repo: { fork: any }) => !repo.fork).reduce((acc: any, item: { stargazers_count: any }) => {
       return acc + item.stargazers_count
   }, 0)
 
@@ -24,20 +24,20 @@ export default async (req, res) => {
   const starredCount = reposStarred.data.length
 
   // Number of issues 
-  const issueCount = repos.data.filter(repo => !repo.fork).reduce((acc, item) => {
+  const issueCount = repos.data.filter((repo: { fork: any }) => !repo.fork).reduce((acc: any, item: { open_issues: any }) => {
     console.log(item.open_issues)
     return acc + item.open_issues
   }, 0)
 
   // List of repos 
-  const repo_names = repos.data.map(repo => repo.name)
+  const repo_names = repos.data.map((repo: { name: any }) => repo.name)
 
   // Number of repos
   const num_repos = repo_names.length
 
   /* Get the data related to the issues of a repository */ 
   // Loop through the repo names 
-  const issueLinks = repos.data.map(repo => repo.issues_url.slice(0, -9))
+  const issueLinks = repos.data.map((repo: { issues_url: string | any[] }) => repo.issues_url.slice(0, -9))
 
   // Note: Just getting the issue links right now but we can loop through 
   // these and get data about all issues pertaining to public repos
--- gitdash/pages/login.tsx
+++ gitdash/pages/login.tsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { FaGithub } from "react-icons/fa";
-import { Button, Box } from "@chakra-ui/react";
+import {
+  Button,
+  Box,
+  useColorModeValue,
+  Heading,
+  SimpleGrid,
+  VisuallyHidden,
+} from "@chakra-ui/react";
+import React from "react";
 
 export default function Home() {
   const [session, loading] = useSession();
@@ -11,29 +19,53 @@ export default function Home() {
       <Head>
         <title>GitDash</title>
       </Head>
-      {/* {!session && (
-        <>
-          Not signed in <br />
-          <button onClick={() => signIn("github")}>
-            <FaGithub>Sign in with GitHub</FaGithub>
-            Sign in with GitHub
-          </button>
-        </>
-      )}
-      {session && (
-        <>
-          Signed in as {session.user.email} <br />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )} */}
+
       <body>
-        <Button
-          colorScheme="teal"
-          leftIcon={<FaGithub />}
-          onClick={() => signIn("github")}
+        <Box
+          bg={useColorModeValue("gray.50", "inherit")}
+          minH="100vh"
+          py="12"
+          px={{ base: "4", lg: "8" }}
         >
-          Sign in with GitHub
-        </Button>
+          {!session && (
+            <>
+              <Box maxW="md" mx="auto">
+                <Heading textAlign="center" size="xl" fontWeight="extrabold">
+                  Sign in to your account with Github
+                </Heading>
+
+                <SimpleGrid mt="6" columns={1} spacing="3">
+                  <Button
+                    colorScheme="teal"
+                    leftIcon={<FaGithub />}
+                    onClick={() => signIn("github")}
+                  >
+                    Sign in with GitHub
+                  </Button>
+                </SimpleGrid>
+              </Box>
+            </>
+          )}
+          {session && (
+            <>
+              <Box maxW="md" mx="auto">
+                <Heading textAlign="center" size="xl" fontWeight="extrabold">
+                  Signed in as {session.user?.name} <br />{" "}
+                </Heading>
+
+                <SimpleGrid mt="6" columns={1} spacing="3">
+                  <Button
+                    colorScheme="teal"
+                    leftIcon={<FaGithub />}
+                    onClick={() => signOut()}
+                  >
+                    Sign Out
+                  </Button>
+                </SimpleGrid>
+              </Box>
+            </>
+          )}
+        </Box>
       </body>
     </>
   );
