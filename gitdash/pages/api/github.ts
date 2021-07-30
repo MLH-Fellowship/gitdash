b88ea400093833480a38f8f09e44b1306b56f862
Prakhar Rathi <prakharrathi25@gmail.com>
2021-07-30 10:45:47 +0530

    Add content and rename api file
    - Rename github.js to github.ts
    - Add more content to the description

--- gitdash/pages/api/github.ts
+++ gitdash/pages/api/github.ts
@@ -5,7 +5,7 @@ export default async (req, res) => {
     // auth: 
     // github token for a particular user, leaving empty for now
   })
-  const username = "prakharrathi25"    // need to get the input from the user for this  
+  const username = "anandrajaram21"    // need to get the input from the user for this  
 
   // Number of followers
   const followers = await octokit.request(`/users/${username}/followers?per_page=100`)
@@ -23,6 +23,8 @@ export default async (req, res) => {
 
   // List of repos
   const repo_names = repos.data.map(repo => repo.name)
+
+  // console.log(repos) - for API Testing 
 
   // Return the counts
   return res.status(200).json({ stars: starsCount, followers: followerCount, starred: starredCount, all_repos: repo_names})
