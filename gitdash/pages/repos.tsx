dea22b0c2e2ca9b26a8d5e6c546fca8cf3905a54
anandrajaram21 <anandrajaram21@gmail.com>
2021-08-09 16:48:00 +0530

    feat: temporary changes. WIP

--- gitdash/pages/repos.tsx
+++ gitdash/pages/repos.tsx
@@ -1,5 +1,24 @@
+import Sidebar from "../components/sidebar";
+import useSWR from "swr";
+
+async function fetcher(...arg: any) {
+  try {
+    const res = await fetch(arg);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 export default function Repos() {
-    return (
-        <h1>This is the repositories page</h1>
-    )
+  const { data: githubData } = useSWR("/api/github", fetcher);
+  const { data: repoData } = useSWR("/api/all_repos", fetcher);
+
+  console.log(repoData);
+
+  return (
+    <Sidebar pageTitle="Repositories" githubData={githubData}>
+      <h1>This is the repositories page</h1>
+    </Sidebar>
+  );
 }
