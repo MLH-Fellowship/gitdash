17c9bcd32d0cb10eaa4e1a0550ed6ad7fab14a84
Green-Ranger11 <junior.eteuati@gmail.com>
2021-08-11 16:59:25 +1300

    Remove fallback

--- server/API/Startup.cs
+++ server/API/Startup.cs
@@ -58,7 +58,6 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
